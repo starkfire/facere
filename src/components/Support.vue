8256d6a426c5de1eea4865cc3ab53c03c7b845ae
Pocholo <plpantoja49@gmail.com>
2020-09-14 22:58:10 +0800

    feat: add support panel

---
+++ src/components/Support.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="support">
+        <a-row>
+            <a-col :xs="24" :lg="8">
+                <p id="title">Support</p>
+                <p id="description">
+                    Featurist will help you grow your audience as an artist and 
+                    fund your projects.
+                </p>
+                <a id="read-article">
+                    <a-icon id="read" type="read" /> Why be a backer? Read our article. <a-icon id="arrow" type="arrow-right" />
+                </a>
+            </a-col>
+            <a-col :xs="24" :lg="15">
+                <div class="feedback">
+                    <a-row id="feedback-one">
+                        <a-col :xs="24" :lg="6" id="head">
+                            <img src="../assets/img/sgan_5.jpg" />
+                        </a-col>
+                        <a-col :xs="24" :lg="18" id="content">
+                            <p id="name">John Doe</p>
+                            <p id="title">Painter</p>
+                            <p id="comment">
+                                "I already tried tons of platforms where I can showcase my artworks 
+                                but none of these platforms really helped me other than Featurist."
+                            </p>
+                        </a-col>
+                    </a-row>
+                    <a-row id="feedback-two">
+                        <a-col :xs="24" :lg="18" id="content">
+                            <p id="name">Jane Doe</p>
+                            <p id="title">Musician</p>
+                            <p id="comment">
+                                "It's actually astonishing that Featurist is much more effective at
+                                growing your audience than any other services."
+                            </p>
+                        </a-col>
+                        <a-col :xs="24" :lg="6" id="head">
+                            <img src="../assets/img/sgan_3.jpg" />
+                        </a-col>
+                    </a-row>
+                    <a-row id="feedback-three">
+                        <a-col :xs="24" :lg="6" id="head">
+                            <img src="../assets/img/sgan_4.jpg" />
+                        </a-col>
+                        <a-col :xs="24" :lg="18" id="content">
+                            <p id="name">Dylan</p>
+                            <p id="title">Script Kiddie</p>
+                            <p id="comment">
+                                *heavy breathing*
+                            </p>
+                        </a-col>
+                    </a-row>
+                </div>
+            </a-col>
+        </a-row>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Support'
+}
+</script>
+<style lang="scss" scoped>
+
+.support {
+    background: #352d2a;
+    color: #fff;
+    font-family: 'Manrope', sans-serif;
+    padding-left: 40px;
+
+    #title {
+        margin-top: 50%;
+        font-size: 48px;
+        font-weight: 700;
+    }
+
+    #description {
+        font-size: 21px;
+        font-weight: 400;
+        margin-bottom: 2em;
+    }
+    
+    #read-article {
+        padding: 16px 32px;
+        border-radius: 12px;
+        font-weight: 700;
+        color: #ddd;
+        background-color: #444;
+
+        #read {
+            margin-right: 5px;
+        }
+
+        &:hover {
+            background-color: #525252;
+        }
+    }
+}
+
+.feedback {
+    margin: 64px 45px;
+    font-family: 'Manrope', sans-serif;
+
+    #content {
+        #name {
+            margin: 0;
+            font-size: 28px;
+            font-weight: 700;
+        }
+
+        #title {
+            margin: 0;
+            font-size: 18px;
+            font-weight: 700;
+            color: #ddd;
+        }
+
+        #comment {
+            margin-top: 15px;
+            font-family: 'Playfair Display', serif;
+            font-weight: 400;
+            font-size: 18px;
+        }
+    }
+
+    %feedback-template {
+        margin: 36px 0px;
+
+        img {
+            border-radius: 50%;
+            height: 150px;
+        }
+    }
+
+    #feedback-one {
+        @extend %feedback-template;
+    }
+
+    #feedback-two {
+        @extend %feedback-template;
+    }
+
+    #feedback-three {
+        @extend %feedback-template;
+    }
+
+    &:first-child {
+        margin-top: 96px;
+    }
+
+    &:last-child {
+        margin-bottom: 96px;
+    }
+}
+
+</style>
