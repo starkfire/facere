148cadba3c2231eb7cc62ffde9fc9a80a63f6ac5
Pocholo <plpantoja49@gmail.com>
2020-09-15 22:43:54 +0800

    feat: add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,101 @@
+<template>
+	<div class="navbar">
+		<a-row>
+			<a-col :xs="24" :lg="6" :span="6" id="logo">
+				<router-link to="/">Facere</router-link>
+			</a-col>
+			<a-col :xs="18" :lg="12" id="nav-buttons">
+				<ul>
+					<li><a href="#">Galleries</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">Donate</a></li>
+					<li><a href="#">About</a></li>
+				</ul>
+			</a-col>
+			<a-col :xs="6" :lg="6" id="highlight">
+				<a href="#" id="auth">Signup</a>
+				<a href="#" id="auth">Login</a>
+			</a-col>
+		</a-row>
+	</div>
+</template>
+<script>
+export default {
+	name: 'Navbar'
+}
+</script>
+<style lang="scss" scoped>
+
+.navbar {
+	width: 100%;
+	min-height: 48px;
+
+	a {
+		display: inline-block;
+	}
+
+	#logo {
+		display: inline-block;
+		text-align: center;
+
+		a {
+			font-family: 'Playfair Display', serif;
+			font-size: 36px;
+			// color: white;
+			color: #000;
+		}
+	}
+
+	#nav-buttons {
+		ul {
+			display: table;
+			list-style: none;
+			text-align: center;
+			position: absolute;
+		}
+
+		li {
+			display: table-cell;
+		}
+
+		a {
+			display: inline-block;
+			position: relative;
+			margin: 20px 24px 0px 24px;
+			text-transform: uppercase;
+			font-family: 'Manrope', sans-serif;
+			font-weight: 700;
+			letter-spacing: 1.4px;
+			// color: #fff;
+			color: #000;
+
+			&:hover {
+				color: #fac999;
+				transition-duration: 0.2s;
+			}
+		}
+	}
+
+	#highlight {
+		$x-pad: 16px;
+		$y-pad: 4px;
+
+		#auth {
+			margin: (20px-$y-pad) (24px-$x-pad) (20px-$y-pad) (24px-$x-pad);
+			font-family: 'Manrope', sans-serif;
+			font-weight: 700;
+			letter-spacing: 1.4px;
+			text-transform: uppercase;
+			font-size: 16px;
+			background-color: #f7b879;
+			padding: $y-pad $x-pad $y-pad $x-pad;
+			color: white;
+
+			&:hover {
+				background-color: #fcac5d;
+			}
+		}
+	}
+}
+
+</style>
