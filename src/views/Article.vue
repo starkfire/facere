06b561e6d8ca8a1b933a64cdf3fe65a3d9f00c5b
Pocholo <plpantoja49@gmail.com>
2020-09-16 23:09:36 +0800

    feat: add Article view

---
+++ src/views/Article.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="article">
+        <Navbar />
+        <div class="article-container">
+            <SampleArticle />
+        </div>
+        <Footer />
+    </div>
+</template>
+<script>
+import Navbar from '../components/Navbar.vue'
+import SampleArticle from '../components/SampleArticle.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+    name: 'Article',
+    components: {
+        Navbar,
+        SampleArticle,
+        Footer
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&family=Source+Serif+Pro:wght@300;400;600&display=swap');
+</style>
