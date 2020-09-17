3212aabf1a6a6e96bff68e75776b51a08b6c94ab
Pocholo <plpantoja49@gmail.com>
2020-09-17 13:08:50 +0800

    feat: add concepts panel

---
+++ src/components/Concepts.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="concepts">
+        <div class="text">
+            <p id="title">
+                Share your concepts to the world
+            </p>
+            <p id="description">
+                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt.
+            </p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Concepts'
+}
+</script>
+<style lang="scss" scoped>
+
+.concepts {
+    min-height: 640px;
+
+    background-image: url('../assets/img/pexels-luis-quintero-1713953.jpg');
+    background-size: cover;
+    background-position: 50% 50%;
+    background-attachment: fixed;
+}
+
+.text {
+    min-height: 640px;
+    background-color: rgba(0, 0, 0, 0.3);
+    text-align: center;
+    padding: 18% 20% 15% 20%;
+    font-family: 'Manrope', sans-serif;
+    color: #fff;
+
+    #title {
+        font-size: 48px;
+        font-weight: 700;
+    }
+
+    #description {
+        font-size: 24px;
+        font-weight: 300;
+    }
+}
+
+</style>
